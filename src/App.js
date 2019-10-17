3e13d870dbb62c704bef6a28fbd58d824acdab84
Sarah Davidsen <sarahdavidsen@desktop-gs0tdbk.kea.local>
2019-10-17 10:33:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SwipeableList, SwipeableListItem } from "@sandstreamdev/react-swipeable-list";
+import "@sandstreamdev/react-swipeable-list/dist/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <SwipeableList>
+          <SwipeableListItem
+            swipeLeft={{
+              content: <div className="list2">Revealed content during swipe</div>,
+              action: () => console.info("swipe action triggered")
+            }}
+            swipeRight={{
+              content: <div className="list2">Revealed content during swipe</div>,
+              action: () => console.info("swipe action triggered")
+            }}
+          >
+            <div className="list1">Item name</div>
+          </SwipeableListItem>
+        </SwipeableList>
+      </div>
     </div>
   );
 }
